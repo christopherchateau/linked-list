cd86949692f6cbcd7742ebeb444b08ee18abafa9
Cierra Washington <cierrajwashi@gmail.com>
2018-07-07 16:52:53 -0600

    Add function to append user's information to page

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 
 var titleInput = document.querySelector(".title-input");
-
 var urlInput = document.querySelector(".url-input");
-
 var websiteTitle = document.querySelector(".website-title")
 var websiteURL = document.querySelector(".website-url")
-
 var enter = document.querySelector(".enter");
 
-enter.addEventListener('click', function(){
+
+// document.body.onload = addElement;
+
+function addElement(){
+
+	document.body.onload = addElement;
+
+	var newDiv = document.createElement("div");
+
+	var input2 = document.createTextNode(urlInput.value);
+	var input1 = document.createTextNode(titleInput.value);
+
+	newDiv.appendChild(input1);
+	newDiv.appendChild(input2);
+
+	var currentDiv = document.getElementById(".article");
+
+	document.body.insertBefore(newDiv, currentDiv);
+
+}
+
+enter.addEventListener('click', addElement);
+
 
 // titleInput = websiteTitle.innerText
-	websiteTitle.innerText = titleInput.value;
-	websiteURL.innerText = urlInput.value;
+	// websiteTitle.innerText = titleInput.value;
+	// websiteURL.innerText = urlInput.value;
 
-});
+	// websiteTitle.append(titleInput.value);
+	// websiteURL.append(urlInput.value);
+	// article.set(".input");
+
+	// var bookmarks = document.querySelector('.bookmark-section');
+
+	// var article = document.createElement("ARTICLE");
+
+	// document.bookmarks.appendChild(article);
+
+	// var newCard = document.createElement("ARTICLE");
+	// var newH1 = document.createTextNode('asdf');
+	// newCard.appendChild(newH1);
+	// var bookmarkSection = document.querySelector(".article");
+	// document.body.insertBefore(newCard, bookmarkSection);
+
 
 document.addEventListener('keyup', function(){
 
@@ -23,3 +57,5 @@ document.addEventListener('keyup', function(){
 	}
 
 });
+
+
