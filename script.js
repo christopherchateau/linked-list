6e3223a794f7441489296f23fb1dd83da38fa675
Chris Chateau <christopherchateau@gmail.com>
2018-07-08 17:58:51 -0600

    Adjusted var names, added syling, media query

--- script.js
+++ script.js
@@ -6,47 +6,48 @@ var websiteURL = document.querySelector(".website-url")
 var enter = document.querySelector(".enter");
 var bookmarks = document.querySelector(".bookmark-section");
 
+	var prevCard;
+
 
 function addElement(){
 
-	var newH2 = document.createElement("h2");
-	var newH4 = document.createElement("h4");
-	var newDiv = document.createElement("article");
+	var newCard = document.createElement("article");
+	var newCardTitle = document.createElement("h2");
+	var newCardUrl = document.createElement("h4");
 
-	newDiv.style.width = '25px';
-	newDiv.style.color = "red";
-	newDiv.style.background = "white"; 
-	newDiv.style.width = "300px";
-	newDiv.style.margin = "0 1em";
-	newDiv.style.border = "1px solid #607d8b";
-	newH4.style.borderBottom = "1px solid red";
-	newH2.style.borderBottom = "1px solid #cfd8dc";
-	newH2.style.padding = "1em";
-	newH4.style.padding = "2em";
-	newH2.style.margin = "1rem 1rem";
-	newH4.style.margin = "1rem 1rem";
+	// newDiv.style.color = "#455A64	";
+	// newDiv.style.background = "white"; 
+	// newDiv.style.width = "90%";
+	// newDiv.style.margin = "0 auto";
+	// newDiv.style.border = "4px solid #607D8B";
+	// newH2.style.borderBottom = "1px solid #CFD8DC";
+	// newH4.style.borderBottom = "1px solid #CFD8DC";
+	// newH2.style.padding = "1em";
+	// newH4.style.padding = "2em";
+	// newH2.style.margin = "1rem 1rem";
+	// newH4.style.margin = "1rem 1rem";
 
 	var input2 = document.createTextNode(urlInput.value);
 	var input1 = document.createTextNode(titleInput.value);
 
-	newH2.appendChild(input1);
-	newH4.appendChild(input2);
+	newCardTitle.appendChild(input1);
+	newCardUrl.appendChild(input2);
 
-	newDiv.appendChild(newH2);
-	newDiv.appendChild(newH4);
+	newCard.appendChild(newCardTitle);
+	newCard.appendChild(newCardUrl);
 
-	var currentDiv = document.getElementById("h1");
-	bookmarks.insertBefore(newDiv, currentDiv);
+	var prevCard = document.querySelector("article");
+	bookmarks.insertBefore(newCard, prevCard);
 }
 
 enter.addEventListener('click', addElement);
 
 document.addEventListener('keyup', function(){
 
-	if((titleInput.value) && (urlInput.value)){
+	if (titleInput.value || urlInput.value){
 		enter.disabled = false;
 	}
-
+	else {
+		enter.disabled = true;
+	}
 });
-
-
