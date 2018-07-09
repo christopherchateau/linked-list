99bc8071e2ced16a6808657f63cdbcabb9d10aa8
Chris Chateau <christopherchateau@gmail.com>
2018-07-09 15:10:26 -0600

    Add styling to read/delete buttons

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ var titleInput = document.querySelector(".title-input");
 var urlInput = document.querySelector(".url-input");
 var websiteTitle = document.querySelector(".website-title")
 var websiteURL = document.querySelector(".website-url")
-var enter = document.querySelector(".enter");
+var enter = document.querySelector(".enter-button");
 var bookmarks = document.querySelector(".bookmark-section");
+var deleteButton;
 
-	var prevCard;
+
+var prevCard;
+
+// deleteButton.addEventListener('click', function(){
+
+// 	alert("Hey!");
+
+
+// });
 
 
 function addElement(e){
@@ -15,20 +24,25 @@ function addElement(e){
 	var newCard = document.createElement("article");
 	var newCardTitle = document.createElement("h2");
 	var newCardUrl = document.createElement("h4");
-	var addReadDeleteTags = document.createElement("button");
+	var readButton = document.createElement("button");
+	deleteButton = document.createElement("button");
 
 	var input1 = document.createTextNode(titleInput.value);
 	var input2 = document.createTextNode(urlInput.value);
 
 	newCardTitle.appendChild(input1);
 	newCardUrl.appendChild(input2);
-	addReadDeleteTags.innerText = 'Read';
 
-	addReadDeleteTags.classList.add("mystyle");
+	readButton.innerText = 'Read';
+	deleteButton.innerText = 'Delete';
+
+	readButton.classList.add("read-button");
+	deleteButton.classList.add("delete-button");
 
 	newCard.appendChild(newCardTitle);
 	newCard.appendChild(newCardUrl);
-	newCard.appendChild(addReadDeleteTags);
+	newCard.appendChild(readButton);
+	newCard.appendChild(deleteButton);
 
 
 	var prevCard = document.querySelector("article");
@@ -46,14 +60,3 @@ document.addEventListener('keyup', function(){
 		enter.disabled = true;
 	}
 });
-	// newDiv.style.color = "#455A64	";
-	// newDiv.style.background = "white"; 
-	// newDiv.style.width = "90%";
-	// newDiv.style.margin = "0 auto";
-	// newDiv.style.border = "4px solid #607D8B";
-	// newH2.style.borderBottom = "1px solid #CFD8DC";
-	// newH4.style.borderBottom = "1px solid #CFD8DC";
-	// newH2.style.padding = "1em";
-	// newH4.style.padding = "2em";
-	// newH2.style.margin = "1rem 1rem";
-	// newH4.style.margin = "1rem 1rem";
