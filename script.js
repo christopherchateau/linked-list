5746171ff9885dbe35c280cf55c829c7d5d77cf7
Chris Chateau <christopherchateau@gmail.com>
2018-07-09 14:11:47 -0600

    Added syling to media query and 'read tag to cards'

--- script.js
+++ script.js
@@ -9,32 +9,27 @@ var bookmarks = document.querySelector(".bookmark-section");
 	var prevCard;
 
 
-function addElement(){
+function addElement(e){
+	e.preventDefault();
 
 	var newCard = document.createElement("article");
 	var newCardTitle = document.createElement("h2");
 	var newCardUrl = document.createElement("h4");
+	var addReadDeleteTags = document.createElement("button");
 
-	// newDiv.style.color = "#455A64	";
-	// newDiv.style.background = "white"; 
-	// newDiv.style.width = "90%";
-	// newDiv.style.margin = "0 auto";
-	// newDiv.style.border = "4px solid #607D8B";
-	// newH2.style.borderBottom = "1px solid #CFD8DC";
-	// newH4.style.borderBottom = "1px solid #CFD8DC";
-	// newH2.style.padding = "1em";
-	// newH4.style.padding = "2em";
-	// newH2.style.margin = "1rem 1rem";
-	// newH4.style.margin = "1rem 1rem";
-
-	var input2 = document.createTextNode(urlInput.value);
 	var input1 = document.createTextNode(titleInput.value);
+	var input2 = document.createTextNode(urlInput.value);
 
 	newCardTitle.appendChild(input1);
 	newCardUrl.appendChild(input2);
+	addReadDeleteTags.innerText = 'Read';
+
+	addReadDeleteTags.classList.add("mystyle");
 
 	newCard.appendChild(newCardTitle);
 	newCard.appendChild(newCardUrl);
+	newCard.appendChild(addReadDeleteTags);
+
 
 	var prevCard = document.querySelector("article");
 	bookmarks.insertBefore(newCard, prevCard);
@@ -51,3 +46,14 @@ document.addEventListener('keyup', function(){
 		enter.disabled = true;
 	}
 });
+	// newDiv.style.color = "#455A64	";
+	// newDiv.style.background = "white"; 
+	// newDiv.style.width = "90%";
+	// newDiv.style.margin = "0 auto";
+	// newDiv.style.border = "4px solid #607D8B";
+	// newH2.style.borderBottom = "1px solid #CFD8DC";
+	// newH4.style.borderBottom = "1px solid #CFD8DC";
+	// newH2.style.padding = "1em";
+	// newH4.style.padding = "2em";
+	// newH2.style.margin = "1rem 1rem";
+	// newH4.style.margin = "1rem 1rem";
